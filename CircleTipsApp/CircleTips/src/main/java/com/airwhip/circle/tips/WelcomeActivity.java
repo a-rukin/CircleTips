514f4807b3cc240ac6b84b94177bec8d00e0eff4
Ivan Samborskiy <whiplash1993@yandex.ru>
2014-03-04 22:24:32 +0400

    added progress bar and is styled

--- CircleTipsApp/CircleTips/src/main/java/com/airwhip/circle/tips/WelcomeActivity.java
+++ CircleTipsApp/CircleTips/src/main/java/com/airwhip/circle/tips/WelcomeActivity.java
@@ -2,12 +2,23 @@ package com.airwhip.circle.tips;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class WelcomeActivity extends Activity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.welcome_activity);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setProgress(100);
     }
 }
